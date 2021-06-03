307e2fc419c39b614126b59f35a7967ed66ac8dc
achille-gohin <achille.gohin@gmail.com>
2021-06-03 15:22:17 +0200

    Add currency

--- src/index.js
+++ src/index.js
@@ -10,9 +10,9 @@ const request_data = async data => {
     .then((res)=>{
     console.log("inside")
     console.log(res.data.price)      
-    price.textContent = Number(res.data.price).toFixed(3);
-    MarketCap.textContent = Number(res.data.market_cap).toFixed(1);
-    TotalBurned.textContent = Number(res.data.total_burned).toFixed(1);
+    price.textContent = Number(Number(res.data.price).toFixed(3)).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    MarketCap.textContent = Number(Number(res.data.market_cap).toFixed(1)).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    TotalBurned.textContent = Number(Number(res.data.total_burned).toFixed(1)).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
 
     })
     .catch((error) => {
