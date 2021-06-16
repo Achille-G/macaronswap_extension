f0dbba4b69420c72774a7d28f1c1228d6182f090
achille-gohin <achille.gohin@gmail.com>
2021-06-16 16:04:17 +0200

    Review TVL

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import axios from "axios";
 const price = document.querySelector(".price");
 const MarketCap = document.querySelector(".MarketCap");
 const TotalBurned = document.querySelector(".TotalBurned");
+const tvl = document.querySelector(".tvl");
+
 
 
 const request_data = async data => {
@@ -21,9 +23,26 @@ const request_data = async data => {
 
 }
 
+const request_data_tvl = async data => {
+    console.log("enter")
+    await axios.get('https://www.macaronswap.finance/api/tvl')
+    .then((res)=>{
+    console.log("inside tvl")
+    console.log(res.data.tvl)      
+    tvl.textContent = Number(Number(res.data.tvl).toFixed(3)).toLocaleString('en-US', { style: 'currency', currency: 'USD' });
+    
+
+    })
+    .catch((error) => {
+    console.error(error);
+    });
+
+}
+
 const handSubmit = async e =>{
     e.preventDefault();
     request_data();
+    request_data_tvl();
     console.log(price)
 }
 
